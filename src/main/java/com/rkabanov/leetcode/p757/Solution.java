161661867803c87da9b8c18c846c7f51397733c6
Roman Kabanov <4685931+unicoder88@users.noreply.github.com>
2025-11-21 14:31:47 +0100

    Second solution - 54/117 passed

--- src/main/java/com/rkabanov/leetcode/p757/Solution.java
+++ src/main/java/com/rkabanov/leetcode/p757/Solution.java
@@ -1,53 +1,65 @@
 package com.rkabanov.leetcode.p757;
 
-import java.util.Comparator;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class Solution {
     public int intersectionSizeTwo(int[][] intervals) {
         // 3000 intervals
 
-        // sort by interval start for easy navigation
-        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
-        System.out.println("Sorted " + Arrays.deepToString(intervals));
-
-        // first interval definitely adds _some_ 2 new numbers
-        int result = 2;
-
-        // N^2:
-        // - for each interval, check all other intervals for 2 or 1 intersection:
-        for (int currentIntervalNum = 1; currentIntervalNum < intervals.length; currentIntervalNum++) {
-            int[] x = intervals[currentIntervalNum];
-            int bestIntersects = 0;
-            for (int otherIntervalNum = 0; otherIntervalNum < intervals.length; otherIntervalNum++) {
-                if (currentIntervalNum == otherIntervalNum) {
-                    continue;
-                }
-
-                // - 2 intersection found - add 0. remove that other interval from the list?
-                // - best 1 intersection - add 1 new
-                // - nothing found - add 2 new
-                int[] y = intervals[otherIntervalNum];
-                int intersects = totalIntersects(x, y);
-                intersects = Math.min(intersects, 2);
-
-                if (intersects > bestIntersects) {
-                    bestIntersects = intersects;
-                }
+        // overlap all intervals and count each number frequency
+        HashMap<Integer, Integer> histogram = new HashMap<>();
+        for (int[] interval : intervals) {
+            for (int num = interval[0]; num <= interval[1]; num++) {
+                histogram.merge(num, 1, Integer::sum);
             }
+        }
+        System.out.println("Histogram: " + histogram.toString());
 
-            result += 2 - bestIntersects;
+        HashSet<Integer> resultNumbers = new HashSet<>();
+        int minResultLength = 0;
+
+        // for each interval, pick 2 most frequent numbers
+        for (int[] interval : intervals) {
+            int[] bestNumbers = twoMostFrequentNumbers(interval, histogram);
+            // 2, 3
+            System.out.println("[" + interval[0] + ", " + interval[1] + "] Best numbers: " + bestNumbers[0] + ", " + bestNumbers[1]);
+            // add to set, increase by how many new numbers were added
+            int sizeBefore = resultNumbers.size();
+            resultNumbers.add(bestNumbers[0]);
+            resultNumbers.add(bestNumbers[1]);
+            minResultLength += resultNumbers.size() - sizeBefore;
         }
 
-        return result;
+        return minResultLength;
     }
 
-    private int totalIntersects(int[] a, int[] b) {
-        // [1, 4] vs [3, 4] = 2
-        // [9, 10] vs [20, 24] = 0
-        int commonFrom = Math.max(a[0], b[0]);
-        int commonTo = Math.min(a[1], b[1]);
+    int[] twoMostFrequentNumbers(int[] interval, Map<Integer, Integer> histogram) {
+        // best numbers: "1" with score 2, "2" with score 3
+        int from = interval[0];
+        int to = interval[1];
 
-        return Math.max(0, commonTo - commonFrom + 1);
+        // pick first 2 numbers as best and second best, in correct order
+        int best = from;
+        int secondBest = from + 1;
+        // fix initial order
+        if (histogram.get(best) < histogram.get(secondBest)) {
+            best = from + 1;
+            secondBest = from;
+        }
+
+        // go over rest of numbers, update best and second best if met
+        for (int num = from + 2; num <= to; num++) {
+            int score = histogram.get(num);
+            if (score > histogram.get(best)) {
+                secondBest = best;
+                best = num;
+            } else if (score > histogram.get(secondBest)) {
+                secondBest = num;
+            }
+        }
+
+        return new int[]{best, secondBest};
     }
 }
